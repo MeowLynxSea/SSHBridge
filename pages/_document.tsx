feaed4263d093b4e979e1b073dca05e7e15c3238
MeowLynxSea <mew@meowdream.cn>
2025-12-23 17:29:11 +0800

    security: remove external RemixIcon CSS include

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -3,12 +3,7 @@ import { Html, Head, Main, NextScript } from 'next/document';
 export default function Document() {
   return (
     <Html lang="en">
-      <Head>
-        <link
-          href="https://cdn.jsdelivr.net/npm/remixicon@3.5.0/fonts/remixicon.css"
-          rel="stylesheet"
-        ></link>
-      </Head>
+      <Head />
       <body>
         <Main />
         <NextScript />
